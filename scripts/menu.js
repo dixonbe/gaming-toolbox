75761b314d5c39eea53be0f477561920fddf75bd
Ben <dixonbe@gmail.com>
2023-12-13 17:46:18 +1100

    Add files via upload

---
+++ scripts/menu.js
@@ -0,0 +1,42 @@
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener("click", function(e) {
+        // Check if the clicked element is the sidebar toggle button
+        if (e.target.classList.contains("bxs-ghost")) {
+            let sidebar = document.querySelector(".sidebar");
+            sidebar.classList.toggle("close");
+        }
+
+        // Check if the clicked element is the sidebar toggle button
+        if (e.target.classList.contains("bxs-game")) {
+            let sidebar = document.querySelector(".sidebar");
+            sidebar.classList.toggle("close");
+        }
+
+        // Check if the clicked element is an arrow
+        if (e.target.classList.contains("arrow")) {
+            let arrowParent = e.target.parentElement.parentElement;
+            arrowParent.classList.toggle("showMenu");
+        }
+    });
+});
+
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener("click", function(e) {
+        // Check if the clicked element is the sidebar toggle button
+        if (e.target.classList.contains("bxs-ghost")) {
+            let sidebar = document.querySelector(".navbar");
+            sidebar.classList.toggle("close");
+        }
+    });
+});
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.addEventListener("click", function(e) {
+        // Check if the clicked element is the sidebar toggle button
+        if (e.target.classList.contains("bxs-game")) {
+            let sidebar = document.querySelector(".navbar");
+            sidebar.classList.toggle("close");
+        }
+    });
+});
