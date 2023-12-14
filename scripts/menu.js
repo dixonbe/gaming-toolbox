43485f4dc38b40aebfb6db5147a39ed2b44238b3
Ben <dixonbe@gmail.com>
2023-12-14 19:06:56 +1100

    Update menu.js

--- scripts/menu.js
+++ scripts/menu.js
@@ -13,23 +13,14 @@ document.addEventListener('DOMContentLoaded', function () {
         }
 
         // Check if the clicked element is an arrow
-        if (e.target.classList.contains("arrow")) {
-            let arrowParent = e.target.parentElement.parentElement;
+        if (e.target.classList.contains("link_name")) {
+            let arrowParent = e.target.parentElement.parentElement.parentElement;
             arrowParent.classList.toggle("showMenu");
         }
     });
 });
 
 
-document.addEventListener('DOMContentLoaded', function () {
-    document.addEventListener("click", function(e) {
-        // Check if the clicked element is the sidebar toggle button
-        if (e.target.classList.contains("bxs-ghost")) {
-            let sidebar = document.querySelector(".navbar");
-            sidebar.classList.toggle("close");
-        }
-    });
-});
 
 document.addEventListener('DOMContentLoaded', function () {
     document.addEventListener("click", function(e) {
