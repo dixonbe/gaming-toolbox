eed483820549142fe69ac753dbe3da032bfd6803
Ben <dixonbe@gmail.com>
2023-12-16 16:35:56 +1100

    Add files via upload

---
+++ style/scripts/gamingtax.js
@@ -0,0 +1,187 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Set default values for each task
+    document.getElementById('turnover').value = (0.00).toFixed(2);
+    document.getElementById('progressivePayments').value = (0.00).toFixed(2);
+    calculateMeteredNet()
+    calculateTaxPayable()
+    calculateTotalDeductions()
+
+});
+    
+    
+    function calculateMeteredNet() {
+    var turnover = parseFloat(document.getElementById("turnover").value) || 0;
+    var totalWins = parseFloat(document.getElementById("totalWins").value) || 0;
+
+    var meteredNet = (turnover - totalWins).toFixed(2);
+    document.getElementById("meteredNet").innerText = meteredNet;
+
+    calculatenetprofit();
+}
+
+function calculateTotalDeductions() {
+    var progressivePayments = parseFloat(document.getElementById("progressivePayments").value) || 0;
+    var ccceJackpots = parseFloat(document.getElementById("ccceJackpots").value) || 0;
+
+    var totalDeductions = (progressivePayments + ccceJackpots).toFixed(2);
+    document.getElementById("totalDeductions").innerText = totalDeductions;
+
+    calculatenetprofit();
+}
+
+function calculatenetprofit() {
+    var meteredNet = parseFloat(document.getElementById("meteredNet").innerText) || 0;
+    var totalDeductions = parseFloat(document.getElementById("totalDeductions").innerText) || 0;
+
+    var netprofit = (meteredNet - totalDeductions).toFixed(2);
+    document.getElementById("netprofit").innerText = netprofit;
+
+    calculateTaxPayable(netprofit);
+	calculatetaxrate1(netprofit);
+	calculatetaxrate2(netprofit);
+	calculatetaxrate3(netprofit);
+	calculatetaxrate4(netprofit);
+	calculatetaxrate5(netprofit);
+	calculatetaxrate6(netprofit);
+	calculatecategory1(netprofit);
+	calculatecategory2(netprofit);
+	calculatecategory3(netprofit);
+}
+
+function calculateTaxPayable(netprofit) {
+    var taxPayable = 0;
+
+    if (netprofit <= 250000) {
+        taxPayable = "$0.00";
+    } else if (netprofit <= 450000) {
+        taxPayable = "$" + ((netprofit - 250000) * 0.2805).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else if (netprofit <= 1250000) {
+        taxPayable = "$" + ((netprofit - 450000) * 0.1805 + 56100).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else if (netprofit <= 2500000) {
+        taxPayable = "$" + ((netprofit - 1250000) * 0.2255 + 200500).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else if (netprofit <= 5000000) {
+        taxPayable = "$" + ((netprofit - 2500000) * 0.2455 + 482375).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else {
+        taxPayable = "$" + ((netprofit - 5000000) * 0.2655 + 1096125).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    document.getElementById("taxPayable").innerText = taxPayable;
+	document.getElementById("taxPayabletotal").innerText = taxPayable;
+}
+
+function calculatetaxrate1(netprofit) {
+    var taxrate1 = 0;
+
+    if (netprofit <= 250000) {
+        taxrate1 = "$0.00";
+    } else {
+        taxrate1 = "$" + (netprofit * 0.0).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    document.getElementById("taxrate1").innerText = taxrate1;
+}
+
+function calculatetaxrate2(netprofit) {
+    var taxrate2 = 0;
+
+    if (netprofit <= 250000) {
+        taxrate2 = "$0.00";
+    } else if (netprofit <= 450000) {
+        taxrate2 = "$" + ((netprofit - 250000) * 0.2805).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else {
+        taxrate2 = "$56,100.00";
+    }
+
+    document.getElementById("taxrate2").innerText = taxrate2;
+}
+
+
+function calculatetaxrate3(netprofit) {
+    var taxrate3 = 0;
+
+    if (netprofit <= 450000) {
+        taxrate3 = "$0.00";
+    } else if (netprofit <= 1250000) {
+        taxrate3 = "$" + ((netprofit - 450000) * 0.1805).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else {
+        taxrate3 = "$144,400.00";
+    }
+
+    document.getElementById("taxrate3").innerText = taxrate3;
+}
+
+function calculatetaxrate4(netprofit) {
+    var taxrate4 = 0;
+
+    if (netprofit <= 1250000) {
+        taxrate4 = "$0.00";
+    } else if (netprofit <= 2500000) {
+        taxrate4 = "$" + ((netprofit - 1250000) * 0.2255).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else {
+        taxrate4 = "$281,875.00";
+    }
+
+    document.getElementById("taxrate4").innerText = taxrate4;
+}
+
+function calculatetaxrate5(netprofit) {
+    var taxrate5 = 0;
+
+    if (netprofit <= 2500000) {
+        taxrate5 = "$0.00";
+    } else if (netprofit <= 5000000) {
+        taxrate5 = "$" + ((netprofit - 2500000) * 0.2455).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    } else {
+        taxrate5 = "$613,750.00";
+    }
+
+    document.getElementById("taxrate5").innerText = taxrate5;
+}
+
+function calculatetaxrate6(netprofit) {
+    var taxrate6 = 0;
+
+    if (netprofit <= 5000000) {
+        taxrate6 = "$0.00";
+    } else {
+        taxrate6 = "$" + ((netprofit - 5000000) * 0.2655).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    document.getElementById("taxrate6").innerText = taxrate6;
+}
+
+function calculatecategory1(netprofit) {
+    var category1 = 0;
+
+    if (netprofit <= 250000) {
+        category1 = "$0.00";
+    } else {
+        category1 = "$" + ((netprofit - 250000) * 0.0075).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    document.getElementById("category1").innerText = category1;
+}
+
+function calculatecategory2(netprofit) {
+    var category2 = 0;
+
+    if (netprofit <= 250000) {
+        category2 = "$0.00";
+    } else {
+        category2 = "$" + ((netprofit - 250000) * 0.0110).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    document.getElementById("category2").innerText = category2;
+}
+
+function calculatecategory3(netprofit) {
+    var category1 = 0;
+
+    if (netprofit <= 250000) {
+        category3 = "$0.00";
+    } else {
+        category3 = "$" + ((netprofit - 250000) * 0.0040).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    }
+
+    document.getElementById("category3").innerText = category3;
+}
---
+++ style/scripts/premium-loyalty.js
@@ -0,0 +1,111 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    document.getElementById('egmcount').value = 100;
+    calculateegmcount();
+    calculatemodule1Cost();
+    
+    document.getElementById('module1count').value = 100;
+    calculatemodule1Cost();
+
+    document.getElementById('module2count').value = 501;
+    calculatemodule2Cost();
+
+    document.getElementById('module3count').value = 200;
+    calculatemodule3Cost();
+    
+});
+
+
+function calculatemodule1Cost() {
+    var egmCount = parseFloat(document.getElementById("egmcount").value) || 0;
+    var serviceFee = getServiceFee(document.getElementById("module1count").value);
+
+    var cost = egmCount * serviceFee * 30;
+
+    document.getElementById("module1cost").innerText = cost.toFixed(2);
+}
+
+function getServiceFee(module1Count) {
+    // Add your logic to determine the service fee based on module1Count
+    if (module1Count >= 1 && module1Count <= 5) {
+        return 0.01; // 1 cent
+    } else if (module1Count >= 6 && module1Count <= 10) {
+        return 0.05; // 5 cents
+    } else if (module1Count >= 11 && module1Count <= 30) {
+        return 0.10; // 10 cents
+    } else if (module1Count >= 31 && module1Count <= 100) {
+        return 0.15; // 15 cents
+    } else if (module1Count >= 101 && module1Count <= 500) {
+        return 0.20; // 20 cents
+    } else {
+        return 0.25; // 25 cents for 501+
+    }
+}
+
+
+function calculatemodule2Cost() {
+    var egmCount = parseFloat(document.getElementById("egmcount").value) || 0;
+    var serviceFee = getServiceFee(document.getElementById("module2count").value);
+
+    var cost = egmCount * serviceFee * 30;
+
+    document.getElementById("module2cost").innerText = cost.toFixed(2);
+}
+
+function getServiceFee(module2Count) {
+    // Add your logic to determine the service fee based on module2Count
+    if (module2Count >= 1 && module2Count <= 5) {
+        return 0.01; // 1 cent
+    } else if (module2Count >= 6 && module2Count <= 10) {
+        return 0.05; // 5 cents
+    } else if (module2Count >= 11 && module2Count <= 30) {
+        return 0.10; // 10 cents
+    } else if (module2Count >= 31 && module2Count <= 100) {
+        return 0.15; // 15 cents
+    } else if (module2Count >= 101 && module2Count <= 500) {
+        return 0.20; // 20 cents
+    } else {
+        return 0.25; // 25 cents for 501+
+    }
+}
+
+
+function calculatemodule3Cost() {
+    var egmCount = parseFloat(document.getElementById("egmcount").value) || 0;
+    var serviceFee = getServiceFee(document.getElementById("module3count").value);
+
+    var cost = egmCount * serviceFee * 30;
+
+    document.getElementById("module3cost").innerText = cost.toFixed(2);
+}
+
+function getServiceFee(module3Count) {
+    // Add your logic to determine the service fee based on module3Count
+    if (module3Count >= 1 && module3Count <= 5) {
+        return 0.01; // 1 cent
+    } else if (module3Count >= 6 && module3Count <= 10) {
+        return 0.05; // 5 cents
+    } else if (module3Count >= 11 && module3Count <= 30) {
+        return 0.10; // 10 cents
+    } else if (module3Count >= 31 && module3Count <= 100) {
+        return 0.15; // 15 cents
+    } else if (module3Count >= 101 && module3Count <= 500) {
+        return 0.20; // 20 cents
+    } else {
+        return 0.25; // 25 cents for 501+
+    }
+}
+
+function calculateegmcount() {
+    var egmcount = parseFloat(document.getElementById("egmcount").value) || 0;
+
+
+    var maxmodule = ((egmcount * 0.25)*30).toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+    document.getElementById("maxmodule1").innerText = maxmodule;
+    document.getElementById("maxmodule2").innerText = maxmodule;
+    document.getElementById("maxmodule3").innerText = maxmodule;
+
+    calculatemodule1Cost();
+    calculatemodule2Cost();
+    calculatemodule3Cost();
+}
